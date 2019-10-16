8384a53af25e8f05275800c0a45e62d0ff327f42
wenlei <906447432@qq.com>
2019-10-16 10:22:24 +0800

    first commit

---
+++ Application/Runtime/Cache/Home/ee87bce4ca00d946bcef740ed7349928.php
@@ -0,0 +1,12 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>20191015</title>
+	</head>
+	<body>
+		<div>
+			今天是2018年10月16日。
+		</div>
+	</body>
+</html>
